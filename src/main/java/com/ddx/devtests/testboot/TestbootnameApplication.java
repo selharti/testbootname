3952ce8bec3fda90a8e4ba32bf4ab8137dfb821d
selharti <selharti@gmail.com>
2016-06-08 02:17:05 +0000

    UI Localization step 1

--- src/main/java/com/ddx/devtests/testboot/TestbootnameApplication.java
+++ src/main/java/com/ddx/devtests/testboot/TestbootnameApplication.java
@@ -1,12 +1,41 @@
 package com.ddx.devtests.testboot;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class TestbootnameApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestbootnameApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(TestbootnameApplication.class, args);
+		
+		/* for test only
+		System.out.println("STARTING =======================");
+	
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+	        String[] beanNames = applicationContext.getBeanDefinitionNames();
+	        Arrays.sort(beanNames);
+	        for (String beanName : beanNames) {
+	            System.out.println(beanName);
+	        }
+	        System.out.println("BEANS END =======================");
+		*/
+		
 	}
+	/*
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.US);
+	    return slr;
+	}
+	*/
 }
